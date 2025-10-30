2f533aa83ab8768af8d61eb0d1f8c2f66d140d4c
smithson03 <khylin2001@gmail.com>
2025-10-30 13:18:38 +0000

    Ready to deploy

---
+++ src/features/notes/EditNoteForm.jsx
@@ -0,0 +1,169 @@
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice"
+import { useEffect, useState } from "react"
+import { faTrashCan, faSave } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import useAuth from "../../hooks/useAuth"
+
+const EditNoteForm = ({ note, users }) => {
+  const { isManager, isAdmin } = useAuth()
+
+  const [updateNote, {
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }] = useUpdateNoteMutation()
+
+  const [deleteNote, {
+    isSuccess: isDelSuccess,
+    isError: isDelError,
+    error: delError
+  }] = useDeleteNoteMutation()
+
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState(note.title)
+  const [text, setText] = useState(note.text)
+  const [completed, setCompleted] = useState(note.completed)
+  const [userId, setUserId] = useState(note.user)
+
+  useEffect (() => {
+    if(isSuccess || isDelError) {
+      setTitle('') 
+      setText('')
+      setUserId('')
+      navigate('/dash/notes')
+    }
+  }, [isSuccess, isDelSuccess, navigate])
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onTextChanged = e => setText(e.target.value)
+  const onCompletedChanged = e => setCompleted(prev => !prev)
+  const onUserIdChanged = e => setUserId(e.target.value)
+
+  const canSave = [title, text, userId].every(Boolean) && !isLoading
+
+  const onSaveNoteClicked = async () => {
+    if(canSave) {
+      await updateNote({ id: note.id, title, text, completed, user: userId })
+    }
+  }
+
+  const onDeleteNoteClicked = async () => {
+    await deleteNote({ id: note.id })
+  }
+
+  const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+  const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+
+  const options = users.map(user => {
+    return (
+      <option key={user.id} value={user.id}>{user.username}</option> 
+    )
+  })
+
+  const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
+  const validTitleClass = !title ? "form__input--incomplete" : ''
+  const validTextClass = !text ? "form__input--incomplete" : ''
+
+  const errContent = (error?.data?.message || delError?.data?.message) ?? ''
+
+  let deleteButton = null
+  if(isManager || isAdmin) {
+    deleteButton = (
+      <button
+        className="icon-button"
+        title="Delete"
+        onClick={onDeleteNoteClicked}
+      >
+        <FontAwesomeIcon icon={faTrashCan} />
+      </button>
+    )
+  }
+
+  let content
+
+  content = (
+    <>
+      <p className={errClass}>{errContent}</p>
+
+      <form className='form' onSubmit={onSaveNoteClicked}>
+        <div className='form__title-row'>
+          <h2>Edit Note</h2>
+          <div className='form__action-buttons'>
+            <button className='icon-button' title='Save' disabled={!canSave} onClick={onSaveNoteClicked}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+            
+            {deleteButton}
+          </div>
+        </div>
+
+        <label className='form__label' htmlFor='title'>
+          Title: <span className='nowrap'></span>
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id='title'
+          name='title'
+          type='text'
+          autoComplete='off'
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label className='form__label' htmlFor='password'>
+          Text: 
+        </label>
+        <textarea
+          className={`form__input ${validTextClass}`}
+          id='text'
+          name='text'
+          type='text'
+          value={text}
+          onChange={onTextChanged}
+        >
+          
+        </textarea>
+
+        <div className="form__row">
+          <div className="form__divider">
+            <label className='form__label form__checkbox-container' htmlFor='note-completed'>
+            WORK COMPLETE:
+              <input
+                className='form__checkbox'
+                id='note-completed'
+                name='note-completed'
+                type='checkbox'
+                checked={completed}
+                onChange={onCompletedChanged}
+              />
+            </label>
+
+            <label className='form__label' htmlFor='note-username'>
+              ASSIGNED TO:
+            </label>
+            <select
+              id='note-username'
+              name='username'
+              className={`form__select`}
+              value={userId}
+              onChange={onUserIdChanged}
+            >
+              {options}
+            </select>
+          </div>
+          <div className="form__divider">
+            <p className="form__created">Created:<br />{created}</p>
+            <p className="form__updated">Updated:<br />{updated}</p>
+          </div>
+        </div>
+      </form>
+    </>
+
+  )
+
+  return content
+}
+export default EditNoteForm
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+   
+  </StrictMode>,
+)
